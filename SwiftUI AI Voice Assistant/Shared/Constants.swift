f36c5836db4ae2cf5d14aeac90a96efb928089e8
Roy <95645767+RoyNkem@users.noreply.github.com>
2024-04-24 18:58:44 +0100

    Test Keys storage

--- SwiftUI AI Voice Assistant/Shared/Constants.swift
+++ SwiftUI AI Voice Assistant/Shared/Constants.swift
@@ -7,18 +7,17 @@
 
 import Foundation
 
-//sk-proj-UQQfrvxEidL4nukFJk2vT3BlbkFJRiO4W4KmztcoiPdSv92C
+// sk-proj-YSzTMRuBilM96se0IDkaT3BlbkFJuKl6iuFY9Wv0qLKhWzpg
 
 struct Constants {
     
-    static var api = "sk-proj-YSzTMRuBilM96se0IDkaT3BlbkFJuKl6iuFY9Wv0qLKhWzpg"
     
-//    static var apiKey: String {
-//        guard let apiKey = ProcessInfo.processInfo.environment["OPENAI_API_KEY"] else {
-//            fatalError("OpenAI API Key not found in environment variables")
-//        }
-//        
-//        return apiKey
-//    }
+    static var apiKey: String {
+        guard let apiKey = ProcessInfo.processInfo.environment["OPENAI_API_KEY"] else {
+            fatalError("OpenAI API Key not found in environment variables")
+        }
+        
+        return apiKey
+    }
 }
 
