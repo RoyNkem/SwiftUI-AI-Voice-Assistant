94b06ed2b11b87635ca8dec0475d9880e9d73329
Roy <95645767+RoyNkem@users.noreply.github.com>
2024-04-24 13:22:26 +0100

    Add Constants Structure for Key Management - Introduce Constants struct to centralize application keys. - Define apiKey in Constants. - Update code references to use Constants.myKey.

---
+++ SwiftUI AI Voice Assistant/Shared/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  SwiftUI AI Voice Assistant
+//
+//  Created by Roy's MacBook M1 on 24/04/2024.
+//
+
+import Foundation
+
+//sk-proj-UQQfrvxEidL4nukFJk2vT3BlbkFJRiO4W4KmztcoiPdSv92C
+
+struct Constants {
+    static let apiKey = "sk-proj-UQQfrvxEidL4nukFJk2vT3BlbkFJRiO4W4KmztcoiPdSv92C"
+}
---
+++ SwiftUI AI Voice Assistant/ViewModels/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  SwiftUI AI Voice Assistant
+//
+//  Created by Roy's MacBook M1 on 24/04/2024.
+//
+
+import Foundation
